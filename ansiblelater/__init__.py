43d5a7c41cc9b7dbc9a6f92edaaf001616266b10
Robert Kaussow <robert.kaussow@mail.schwarz>
2019-03-25 11:30:41 +0100

    [SKIP CI] prepare new release

--- ansiblelater/__init__.py
+++ ansiblelater/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Robert Kaussow"
 __project__ = "ansible-later"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __license__ = "MIT"
 __maintainer__ = "Robert Kaussow"
 __email__ = "mail@geeklabor.de"
