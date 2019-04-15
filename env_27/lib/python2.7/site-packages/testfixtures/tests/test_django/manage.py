f05d6fd23fb7cbacb7ec2995a90916881dbde97e
Robert Kaussow <robert.kaussow@mail.schwarz>
2019-04-15 12:31:38 +0200

    [SKIP CI] remove python virtual env

--- env_27/lib/python2.7/site-packages/testfixtures/tests/test_django/manage.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testfixtures.tests.test_django.settings")
-    from django.core.management import execute_from_command_line
-    execute_from_command_line()
-
-if __name__ == "__main__": # pragma: no cover
-    main()
