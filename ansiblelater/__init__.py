79398d95b4dc5d2e9569e87ec0e5cae0cbe78028
Robert Kaussow <mail@geeklabor.de>
2020-09-16 15:44:22 +0200

    [skip ci] prepare bugfix release

--- ansiblelater/__init__.py
+++ ansiblelater/__init__.py
@@ -6,7 +6,7 @@ __license__ = "MIT"
 __maintainer__ = "Robert Kaussow"
 __email__ = "mail@thegeeklab.de"
 __url__ = "https://github.com/xoxys/ansible-later"
-__version__ = "0.3.8"
+__version__ = "0.3.9"
 
 from ansiblelater import logger
 
