b6369ef3bc85a2f8c94c60f343e8e0ee2168511c
Robert Kaussow <mail@geeklabor.de>
2020-10-08 00:02:00 +0200

    [skip ci] prepare release

--- ansiblelater/__init__.py
+++ ansiblelater/__init__.py
@@ -6,7 +6,7 @@ __license__ = "MIT"
 __maintainer__ = "Robert Kaussow"
 __email__ = "mail@thegeeklab.de"
 __url__ = "https://github.com/thegeeklab/ansible-later"
-__version__ = "0.3.11"
+__version__ = "0.3.12"
 
 from ansiblelater import logger
 
