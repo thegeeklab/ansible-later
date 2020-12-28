5b0df93d78c777f6dde96af67dd0abbadeca53b8
Robert Kaussow <mail@geeklabor.de>
2020-12-28 15:43:15 +0100

    build: migrate to poetry

--- ansiblelater/__init__.py
+++ ansiblelater/__init__.py
@@ -1,12 +1,6 @@
 """Default package."""
 
-__author__ = "Robert Kaussow"
-__project__ = "ansible-later"
-__license__ = "MIT"
-__maintainer__ = "Robert Kaussow"
-__email__ = "mail@thegeeklab.de"
-__url__ = "https://github.com/thegeeklab/ansible-later"
-__version__ = "0.3.12"
+__version__ = "0.0.0"
 
 from ansiblelater import logger
 
