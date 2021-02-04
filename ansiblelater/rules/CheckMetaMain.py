bd6a7c576f48da0fe4a55e2b8fb55b7f2b170ca9
Robert Kaussow <mail@geeklabor.de>
2021-02-04 21:08:38 +0100

    fix dependencies check in CheckMetaMain

--- ansiblelater/rules/CheckMetaMain.py
+++ ansiblelater/rules/CheckMetaMain.py
@@ -13,12 +13,17 @@ class CheckMetaMain(StandardBase):
 
     def check(self, candidate, settings):
         content, errors = self.get_raw_yaml(candidate, settings)
-        keys = ["author", "description", "min_ansible_version", "platforms", "dependencies"]
+        keys = ["author", "description", "min_ansible_version", "platforms"]
 
         if not errors:
             has_galaxy_info = (isinstance(content, dict) and "galaxy_info" in content.keys())
+            has_dependencies = (isinstance(content, dict) and "dependencies" in content.keys())
+
             if not has_galaxy_info:
                 errors.append(self.Error(None, self.helptext.format(key="galaxy_info")))
+
+            if not has_dependencies:
+                errors.append(self.Error(None, self.helptext.format(key="dependencies")))
 
             for key in keys:
                 if has_galaxy_info and not nested_lookup(key, content.get("galaxy_info", {})):
